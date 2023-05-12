574fbb907107f03b5666fbb37b1e14eb89ec087f
Beknur Smagulov <beknur_smagulov@epam.com>
2023-05-13 00:23:25 +0600

    2 main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("first from main");
+        System.out.println("second from feature");
     }
 }
