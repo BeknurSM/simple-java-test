cb9ba98c13543d05822fb9906c5e242ab1689f41
Beknur Smagulov <beknur_smagulov@epam.com>
2023-05-13 00:37:05 +0600

    deleted all

--- Main.java
+++ Main.java
@@ -1,11 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("first");
-        System.out.println("first from feature");
-        System.out.println("second from feature");
-        System.out.println("third from feature");
-        System.out.println("first from main");
-        System.out.println("second from feature");
-        System.out.println("third from feature");
     }
 }
